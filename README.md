d90c908b35fb01f45b686240efad52cae5e48407
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-01-06 16:34:11 +0800

    Fri Jan  6 04:34:11 PM CST 2023

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-app-Image-to-ICO
 Resize and convert image to ICO format.
+
+- Example file: https://pulipulichen.github.io/docker-app-Image-to-ICO/example%20folder/image%20%E5%9C%96%E7%89%87.tif
+- Windows: https://pulipulichen.github.io/docker-app-Image-to-ICO/bin/image-to-ico.exe
---
+++ app/image-to-ico.js
@@ -0,0 +1,27 @@
+const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
+const GetExistedArgv = require('./lib/GetExistedArgv')
+
+const path = require('path')
+const fs = require('fs')
+
+// convert a.tif -thumbnail 64x64^ -gravity center -extent 64x64 b.ico
+
+let main = async function () {
+  let files = GetExistedArgv()
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    
+    let filename = path.basename(file)
+    let dirname = path.dirname(file)
+    let filenameNoExt = filename
+    if (filenameNoExt.indexOf('.') > -1) {
+      filenameNoExt = filenameNoExt.slice(0, filenameNoExt.lastIndexOf('.'))
+    }
+    // await ShellSpawn(`img2pdf -o "/input/${filenameNoExt}.pdf" ${imgs.join(" ")}`)
+    await ShellExec(`convert "${file}" -flatten -fuzz 1% -trim +repage -thumbnail 64x64^ -gravity center -extent 64x64 "${path.resolve(dirname, filenameNoExt + '.ico')}"`)
+    // convert "a.png" -flatten -fuzz 1% -trim +repage -thumbnail 64x64^ -gravity center -extent 64x64 "b.ico"
+  }
+}
+
+main()
